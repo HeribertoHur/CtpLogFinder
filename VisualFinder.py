936dc2abab60927df933070166fa77d6fb4f2117
Heriberto <heriberto.valle@FII-NA.com>
2022-04-20 13:49:18 -0500

    Multiple Serials Search

--- VisualFinder.py
+++ VisualFinder.py
@@ -3,18 +3,16 @@ from selenium import webdriver
 from openpyxl import Workbook
 from datetime import date
 
-
 PATH = "C:\Driver\chromedriver.exe"
 
 #Variables Declaration
 listOfTesters = []
-counterOfTester = 0
-
 listOfPossibleResults = []
 
 window = Tk()
 window.title("CTP LOG FINDER")
-window.geometry('300x150')
+window.geometry('400x150')
+window.iconbitmap('find.ico')
 
 Thor = ["gdlctp7034/","gdlctp7065/", "gdlctp7033/", "gdlctp7053/", "gdlctp7070/", "gdlctp7035/", "gdlctp7036/", "gdlctp7068/" ]
 CTOATO = ["gdlctp70110/", "gdlctp7027/", "gdlctp7031/", "gdlctp7032/"]
@@ -25,15 +23,15 @@ Oracle = ["gdlctp7013/","gdlctp7042/","gdlctp7061/","gdlctp7062/","gdlctp7122/",
 def findSerial(SerialNumber, project):
     print(SerialNumber, project)
     Logs = getLogs(SerialNumber, getTesters(project))
-    createResultSheet(Logs,SerialNumber)
-    
- 
-def getInitialData():
+    createResultSheet(Logs)
+        
+def getInitialMultipleData():
     #TextBox
-    inputData = serial_entry.get()
+    inputData = MultipleSerial_entry.get()
     #DropDownSelector
-    projectData = clicked.get()    
-    findSerial(inputData,projectData)
+    projectData = clicked.get()
+    arrayData = inputData.split(",")
+    findSerial(arrayData,projectData)
     
 def getTesters(project):
     projectSelected = ""
@@ -46,31 +44,32 @@ def getTesters(project):
             
     return projectSelected
 
-def getLogs(Serial, testers):
+def getLogs(Seriales, testers):
     driver = webdriver.Chrome(PATH)
     counterOfLogs = 0
     for tester in testers:
         driver.get("http://172.17.70.161/ctplogs/" + tester)    ## We gonna search inside of each tester that the project has
         counterOfLogs = counterOfLogs + 1
         logsResults = driver.find_elements_by_tag_name("a")     # Gonna use the same method, use the tags "a"
+        
         for log in logsResults:
             logFile = log.text
             counterOfLogs = counterOfLogs + 1
             if counterOfLogs > 5:
-                if Serial in logFile:                                         # Compare, if the text of the tag "a" contains the serial number
-                    result = "http://172.17.70.161/ctplogs/"+ tester + logFile #Gonna save the Link with the tester and the name 
-                    listOfPossibleResults.append(result)                       # Inside of a list
-        
+                for serial in Seriales:
+                    if serial in logFile:                                         # Compare, if the text of the tag "a" contains the serial number
+                        result = "http://172.17.70.161/ctplogs/"+ tester + logFile #Gonna save the Link with the tester and the name 
+                        listOfPossibleResults.append(result)                       # Inside of a list
     return listOfPossibleResults
      
 # SERIAL EXAMPLE:  SGFGD2214654524             
-def createResultSheet(resultLogs,serial):
+def createResultSheet(resultLogs):
     wb = Workbook()
     ws = wb.active
-    ws.title = serial
+    ws.title = "Examples"
     for log in resultLogs:
         ws.append([log])
-    wb.save(serial + '.xlsx')
+    wb.save("ejemplo" + '.xlsx')
     
 #DropDown Project Selector
 clicked = StringVar()
@@ -78,9 +77,10 @@ clicked.set("Project")
 dropSelector = OptionMenu(window, clicked, "Thor", "CTOATO","Oracle")
 dropSelector.pack()
 
-#InputField
-serial_entry = Entry(window)
-serial_entry.pack(pady=30)
-Button(window, text="Find Serial", padx=10, pady=5,command=getInitialData).pack()
+#InputFieldWithMultipleOption
+MultipleSerial_entry = Entry(window)
+MultipleSerial_entry = Text(height=3,width=40)
+MultipleSerial_entry.pack(pady=5)
+Button(window, text="Find Serial",pady=5,height=1, width=30,command=getInitialMultipleData).pack()
 
 window.mainloop()
